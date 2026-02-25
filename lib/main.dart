d93535917774d0c62ab27784c99f562437b3abf7
Wlahood <wlahood@tiscali.it>
2026-02-25 17:29:28 +0100

    MOBILE

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(body: Center(child: Text('HELLO'))),
-    ),
-  );
+  runApp(const BookaloApp());
+}
+
+class BookaloApp extends StatelessWidget {
+  const BookaloApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bookalo',
+      theme: ThemeData(useMaterial3: true),
+      home: const HomeScreen(),
+    );
+  }
 }
